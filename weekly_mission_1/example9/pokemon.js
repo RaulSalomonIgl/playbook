803372ff36befe324469f7c8cdf6ce1d74c3edc0
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-04-14 01:13:52 -0500

    Add: Example 10 - Exercise

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,10 @@
+// exports a class
+export default class Pokemon{
+    constructor (name){
+        this.name = name
+    }
+
+    sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+}
