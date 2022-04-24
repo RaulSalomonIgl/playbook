167c961fc99617b70549640af005e2e821154654
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-04-24 03:11:40 -0500

    Agregando ejemplo 5 de examples_1

---
+++ weekly_mission_2/examples1/examples1-5.js
@@ -0,0 +1,4 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const modifieNames = names.map((name) => {return name.toLowerCase()})
+console.log(modifieNames)
