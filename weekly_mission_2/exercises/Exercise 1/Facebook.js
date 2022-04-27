95335b51c5c877b42cc94616926bf734291846d1
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-04-27 00:51:13 -0500

    Agregando ejercicio 1: Facebook - Post

--- weekly_mission_2/exercises/Exercise 1/Facebook.js
+++ weekly_mission_2/exercises/Exercise 1/Facebook.js
@@ -17,3 +17,64 @@ console.log("User")
 console.log("Nombre: " + user.name)
 console.log(`Fecha de nacimiento: ${user.getDateOfBirth()}\n`)
 user.getProfile()
+
+
+const post = {
+    user: "Raul Salomon",
+    date: "7 de enero",
+    post:{
+        privacy: "Public",
+        description: "¡Feliz día, hoy lunes 13 de septiembre se festeja el día del programador 🥳!",
+        file: null,
+        tagPerson: [],
+        location: null,
+        'q&a':[],
+        importantEvent: null,
+        gif: null,
+        live: null,
+    },
+    reactions: [
+        ["ME GUSTA",13],
+        ["ME ENCANTA",58],
+        ["ME IMPORTA",1],
+        ["ME DIVIERTE",0],
+        ["ME ASOMBRA",0],
+        ["ME ENTISTECE",0],
+        ["ME ENOJA",0]
+    ],
+    comments: [
+        [
+            "Raul Salomon",
+            "Felicidades!!!",
+            [
+                ["ME GUSTA",1],
+                ["ME ENCANTA",10],
+                ["ME IMPORTA",3],
+                ["ME DIVIERTE",0],
+                ["ME ASOMBRA",0],
+                ["ME ENTISTECE",0],
+                ["ME ENOJA",0]
+            ]
+        ]
+    ],
+    shared: 25,
+    getTotalReactions: function(){
+        return this.reactions.reduce((acc, element) => acc + element[1], 0)
+    },
+    getComments: function(){
+        this.comments.forEach(comment =>
+                console.log(`${comment[0]}
+                 \n${comment[1]}\n${comment[2].reduce((acc, element) => acc + element[1], 0)} reacciones\n
+                `)
+            )
+    },
+    getPost: function(){
+        console.log(`${this.user}   ${this.date}\n${this.post.description}\n${this.getTotalReactions()} reacciones     ${this.comments.length} comentarios\n`)
+        return this.getComments()
+    }
+}
+
+console.log("Reacciones: " + post.getTotalReactions())
+post.getComments()
+console.log()
+post.getPost()
