9fba241c39f843b36973ff06f0d51f6193fbebf9
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-04-27 03:02:36 -0500

    Actualización ejercicio 2: Agregando los procesos requeridos

--- weekly_mission_2/exercises/Exercise 2/Explorers.js
+++ weekly_mission_2/exercises/Exercise 2/Explorers.js
@@ -60,24 +60,45 @@ const explorers = [
    ]
 
    //Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH
-
+console.log("\nImprime el nombre de cada explorer en la lista")
+explorers.forEach(explorer => console.log(explorer.name))
 
    //Imprime el stack de cada explorer, usa FOR EACH
-   
+console.log("\nImprime el stack de cada explorer")
+explorers.forEach(explorer => console.log(`${explorer.name}: ${explorer.stack}`))
    
    //Crea una nueva lista con las listas de stacks de cada explorer, usa MAP
-   
+console.log("\nCrea una nueva lista con las listas de stacks de cada explorer")
+const lstStacks = explorers.map((stack) => {return stack.stack})
+console.log(lstStacks)
    
    //Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)
+console.log("\nObtén la lista de explorers que tengan en su stack 'js'")
+const lstJsInStacks = explorers.filter((stack) => stack.stack.includes('js'))
+console.log(lstJsInStacks)
    
-   
-   //Busca el primer explorer que sea de la CDMX, usa FIND
-   
+   //Busca el primer explorer que sea de la CDMX, usa FIND   
+console.log("\nBusca el primer explorer que sea de la CDMX")
+const explorerCDMX = explorers.find((explorer) => explorer.city === "CDMX")
+console.log(explorerCDMX)
    
    //Obtén la suma de todos los exercises_completed, usa REDUCE
-   
+console.log("\nSuma de todos los exercises_completed: "+ explorers.reduce((acc, element) => acc + element.exercises_completed, 0))
    
    //Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
-   
+console.log("\nObtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true")
+
+if( explorers.some( (explorer) => explorer.missions.frontend.exercisesFinished === true ) ){
+  console.log("Por lo menos un explorer tiene la propiedad 'exercisesFinished' en frontend como true")
+} else{
+  console.log("Ningun explorer cumple con la validación")
+}  
    
    //Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
+console.log("\nObtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true")
+
+if( explorers.every((explorer) => explorer.missions.onboarding.isFinished === true) ){
+  console.log("Todos los explores finalizaron el Onboarding")
+} else{
+  console.log("NO todos los explores finalizaron el Onboarding")
+}  
