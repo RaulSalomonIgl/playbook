73d47a109abc4f6bfd34f23edd61b39c5e4230cd
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-05-20 01:45:56 -0500

    Twitter-Requerimiento 1

---
+++ weekly_mission_3/Twitter/test/models/User.test.js
@@ -0,0 +1,15 @@
+const User = require('./../../app/User')
+
+describe("Unit Tests for User class", () => {
+    test('Create an usa User object', () => {
+        const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+
+    });
+})
