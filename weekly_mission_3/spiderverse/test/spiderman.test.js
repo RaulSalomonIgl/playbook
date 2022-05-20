2fa720c525f91f3a9e6982a5e613770755eb9e36
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-05-20 00:54:31 -0500

    Write a test for the next bit of functionality you want to add

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,15 @@
+const Spiderman = require('./../app/Spiderman')
+
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+      //Instanciando Class Spiderman
+      const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony" )
+
+      //Validamos que el codigo funcione de la manera esperada
+      expect(andrewGarfield.name).toBe("Spiderman Sony")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.movies).toBe(31)
+      expect(andrewGarfield.studio).toBe("Sony")
     });
   })
