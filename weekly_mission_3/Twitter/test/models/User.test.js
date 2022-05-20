d286b40cc48637f01af2c3d12d335ecdc856b854
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-05-20 01:54:33 -0500

    Twitter-Requerimiento 2

--- weekly_mission_3/Twitter/test/models/User.test.js
+++ weekly_mission_3/Twitter/test/models/User.test.js
@@ -1,15 +1,15 @@
 const User = require('./../../app/User')
 
 describe("Unit Tests for User class", () => {
-    test('Create an usa User object', () => {
-        const user = new User(1, "carlogilmar","Carlo","Bio","dateCreated","lastUpdated")
+    test('Create an User object', () => {
+        const user = new User(1, "carlogilmar","Carlo","Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()//Verifica que el valor no sea undefine
+        expect(user.lastUpdated).not.toBeUndefined()
 
     });
 })
