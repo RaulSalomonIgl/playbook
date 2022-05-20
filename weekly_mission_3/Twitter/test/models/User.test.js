72a736ca0f06a6e71f0e6df29f650aeb9e0c3f27
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-05-20 02:02:03 -0500

    Twitter-Requerimiento 3

--- weekly_mission_3/Twitter/test/models/User.test.js
+++ weekly_mission_3/Twitter/test/models/User.test.js
@@ -12,4 +12,11 @@ describe("Unit Tests for User class", () => {
         expect(user.lastUpdated).not.toBeUndefined()
 
     });
+    test('Add getters', () => {
+        const user = new User(1, "carlogilmar","Carlo","Bio")
+        expect(user.getUsername).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    });
 })
