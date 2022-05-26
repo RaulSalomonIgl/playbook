015e94b2b4aa0893e327d1297186cbc0b61b0f0d
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-05-26 00:41:01 -0500

    Sprint 2: Agregar servicios - Requerimiento 2

--- weekly_mission_3/Twitter/app/services/UserService.js
+++ weekly_mission_3/Twitter/app/services/UserService.js
@@ -4,6 +4,10 @@ class UserService{
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+
+    static getInfo(oUser){
+        return Object.values(oUser)
+    }
 }
 
 module.exports = UserService
