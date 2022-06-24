fb4f0c00b45fc382f22fb8144775be8ffce902bb
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-06-23 23:31:13 -0500

    Mi primer API: POST Endpoint que se encarga de crear un explorer

--- weekly_mission_3/express_api/app/app.js
+++ weekly_mission_3/express_api/app/app.js
@@ -26,6 +26,14 @@ app.get('/v1/explorers/:id', (req, res) => {
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`API Explorers POST request ${new Date()}`)
+
+    const requestBody = req.body // Parámetro de un cliente
+    req.params = requestBody
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
