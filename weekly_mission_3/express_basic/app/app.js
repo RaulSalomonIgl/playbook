c8ec5ca5026241dffe3fb709426f2faf00097505
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-06-23 22:35:00 -0500

    Server con Express.js: Agregar nueva ruta para regresar un objeto

--- weekly_mission_3/express_basic/app/app.js
+++ weekly_mission_3/express_basic/app/app.js
@@ -16,6 +16,13 @@ app.get('/launchx', (req, res) => {
     res.send('Bienvenido a launchx')
 })
 
+// Respondiendo un objeto
+// localhost:3000/explorerInNode
+app.get('/explorerInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
 // Inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
